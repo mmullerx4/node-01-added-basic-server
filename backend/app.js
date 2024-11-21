e4ea6841c6c65877b455113db5542097584b9789
mmullerx4 <mmullerx4@gmail.com>
2024-11-20 22:10:14 -0500

    tutorial

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,18 @@ const express = require('express');
 
 const app = express();
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  res.setHeader('Access-Control-Allow-Methods',
+    'GET, POST, PATCH, DELETE, OPTIONS'
+  );
+  next();
+});
+
 app.use('/api/posts',(req, res, next) => {
   const posts = [
     { id: 'fadfadfaa',
@@ -13,7 +25,7 @@ app.use('/api/posts',(req, res, next) => {
       content: 'This is coming from the server!!'
     }
   ];
-  return res.status(200).json({
+  res.status(200).json({
     message: 'Posts fetched successfully!',
     posts: posts
   });
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,13 @@ import { BrowserModule } from "@angular/platform-browser";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
 import { MatInputModule } from '@angular/material/input';
 import { MatCardModule } from "@angular/material/card";
 import { MatButtonModule } from "@angular/material/button";
 import { MatToolbarModule } from "@angular/material/toolbar";
 import { MatExpansionModule } from "@angular/material/expansion";
+
 
 
 
@@ -31,7 +33,8 @@ import { PostListComponent } from "./posts/post-list/post-list.component";
     MatCardModule,
     MatButtonModule,
     MatToolbarModule,
-    MatExpansionModule
+    MatExpansionModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
